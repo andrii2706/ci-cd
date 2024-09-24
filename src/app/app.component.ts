2c986bf7c61fa61cd87921dec562c0e057765cd0
andrii2706 <andriipas200017@gmail.com>
2024-09-24 14:13:50 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {FormsModule} from "@angular/forms";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatTooltip} from "@angular/material/tooltip";
+import {MatLine} from "@angular/material/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCheckbox, MatSidenavContent, MatSidenav, MatSidenavContainer, FormsModule, MatButton, MatIcon, MatIconButton, MatToolbar, MatNavList, MatListItem, MatTooltip, RouterLink, MatLine],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ci-cd-test-project';
+  constructor() {
+  }
+  events: string[] = [];
+  opened: boolean = false;
 }
