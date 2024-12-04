6243a66cf339a944a60b2cfc6b5877583c0a6915
andrii2706 <andriipas200017@gmail.com>
2024-12-04 23:37:39 +0200

    unit test

--- src/app/shared/components/games-cards/games-cards.component.spec.ts
+++ src/app/shared/components/games-cards/games-cards.component.spec.ts
@@ -11,119 +11,127 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('GamesCardsComponent', () => {
-  let component: GamesCardsComponent;
-  let fixture: ComponentFixture<GamesCardsComponent>;
-  let snackBarMock: jest.Mocked<MatSnackBar>;
-  let routerMock: { navigate: jest.Mock };
-  let activatedRouteMock: Partial<ActivatedRoute>;
+	let component: GamesCardsComponent;
+	let fixture: ComponentFixture<GamesCardsComponent>;
+	let snackBarMock: jest.Mocked<MatSnackBar>;
+	let routerMock: { navigate: jest.Mock };
+	let activatedRouteMock: Partial<ActivatedRoute>;
 
-  beforeEach(() => {
-    /* eslint-disable   @typescript-eslint/ban-ts-comment */
-    //@ts-ignore
-    snackBarMock = {
-      openFromComponent: jest.fn(),
-    };
+	beforeEach(() => {
+		/* eslint-disable   @typescript-eslint/ban-ts-comment */
+		//@ts-ignore
+		snackBarMock = {
+			openFromComponent: jest.fn(),
+		};
 
-    routerMock = {
-      navigate: jest.fn(),
-    };
+		routerMock = {
+			navigate: jest.fn(),
+		};
 
-    activatedRouteMock = {};
+		activatedRouteMock = {};
 
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        BrowserAnimationsModule,
-        AppMaterialModule,
-        SpinnerComponent,
-        ReplaceNullImgPipe,
-        GamesCardsComponent
-      ],
-      providers: [
-        { provide: MatSnackBar, useValue: snackBarMock },
-        { provide: Router, useValue: routerMock },
-        { provide: ActivatedRoute, useValue: activatedRouteMock },
-      ],
-      schemas: [NO_ERRORS_SCHEMA],
-    }).compileComponents();
+		TestBed.configureTestingModule({
+			imports: [
+				RouterTestingModule,
+				BrowserAnimationsModule,
+				AppMaterialModule,
+				SpinnerComponent,
+				ReplaceNullImgPipe,
+				GamesCardsComponent,
+			],
+			providers: [
+				{ provide: MatSnackBar, useValue: snackBarMock },
+				{ provide: Router, useValue: routerMock },
+				{ provide: ActivatedRoute, useValue: activatedRouteMock },
+			],
+			schemas: [NO_ERRORS_SCHEMA],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(GamesCardsComponent);
-    component = fixture.componentInstance;
-  });
+		fixture = TestBed.createComponent(GamesCardsComponent);
+		component = fixture.componentInstance;
+	});
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
 
-  describe('ngOnInit', () => {
-    it('should initialize userStatus correctly', () => {
-      const userMock = '{"userId": "123"}';
-      localStorage.setItem('user', userMock);
+	describe('ngOnInit', () => {
+		it('should initialize userStatus correctly', () => {
+			const userMock = '{"userId": "123"}';
+			localStorage.setItem('user', userMock);
 
-      component.ngOnInit();
+			component.ngOnInit();
 
-      expect(component.userStatus).toBe(false); // userStatus should be true if user is present
-    });
+			expect(component.userStatus).toBe(false); // userStatus should be true if user is present
+		});
 
-    it('should set userStatus to false if no user in localStorage', () => {
-      localStorage.removeItem('user');
-      component.ngOnInit();
-      expect(component.userStatus).toBe(false);
-    });
-  });
+		it('should set userStatus to false if no user in localStorage', () => {
+			localStorage.removeItem('user');
+			component.ngOnInit();
+			expect(component.userStatus).toBe(false);
+		});
+	});
 
-  describe('game setter', () => {
-    it('should set gameInfo correctly when game is passed in', () => {
-      const game: Game = { id: 1, name: 'Test Game', released: '2024-01-01' };
-      component.game = game;
+	describe('game setter', () => {
+		it('should set gameInfo correctly when game is passed in', () => {
+			const game: Game = { id: 1, name: 'Test Game', released: '2024-01-01' };
+			component.game = game;
 
-      expect(component.gameInfo).toEqual(game);
-    });
-  });
+			expect(component.gameInfo).toEqual(game);
+		});
+	});
 
-  describe('isGameBought setter', () => {
-    it('should set showLabel to true if game is already bought', () => {
-      const game: Game = { id: 1, name: 'Test Game', released: '2024-01-01' };
-      const boughtGames: Game[] = [{ id: 1, name: 'Test Game', released: '2024-01-01' }];
-      component.game = game;
-      component.isGameBought = boughtGames;
+	describe('isGameBought setter', () => {
+		it('should set showLabel to true if game is already bought', () => {
+			const game: Game = { id: 1, name: 'Test Game', released: '2024-01-01' };
+			const boughtGames: Game[] = [
+				{ id: 1, name: 'Test Game', released: '2024-01-01' },
+			];
+			component.game = game;
+			component.isGameBought = boughtGames;
 
-      expect(component.showLabel).toBe(true);
-    });
+			expect(component.showLabel).toBe(true);
+		});
 
-    it('should not change showLabel if game is not bought', () => {
-      const game: Game = { id: 1, name: 'Test Game', released: '2024-01-01' };
-      const boughtGames: Game[] = [{ id: 2, name: 'Another Game', released: '2024-01-01' }];
-      component.game = game;
-      component.isGameBought = boughtGames;
+		it('should not change showLabel if game is not bought', () => {
+			const game: Game = { id: 1, name: 'Test Game', released: '2024-01-01' };
+			const boughtGames: Game[] = [
+				{ id: 2, name: 'Another Game', released: '2024-01-01' },
+			];
+			component.game = game;
+			component.isGameBought = boughtGames;
 
-      expect(component.showLabel).toBeUndefined();
-    });
-  });
+			expect(component.showLabel).toBeUndefined();
+		});
+	});
 
-  describe('goToGameDetails', () => {
-    it('should navigate to the game details page when in /home route', () => {
-      component.gameInfo = { id: 1, name: 'Test Game', released: '2024-01-01' };
-      routerMock.navigate.mockResolvedValue(true);
-      /* eslint-disable   @typescript-eslint/ban-ts-comment */
-      //@ts-ignore
-      component.router.url = '/home';
+	describe('goToGameDetails', () => {
+		it('should navigate to the game details page when in /home route', () => {
+			component.gameInfo = { id: 1, name: 'Test Game', released: '2024-01-01' };
+			routerMock.navigate.mockResolvedValue(true);
+			/* eslint-disable   @typescript-eslint/ban-ts-comment */
+			//@ts-ignore
+			component.router.url = '/home';
 
-      component.goToGameDetails();
+			component.goToGameDetails();
 
-      expect(routerMock.navigate).toHaveBeenCalledWith(['/games/1'], { relativeTo: activatedRouteMock });
-    });
+			expect(routerMock.navigate).toHaveBeenCalledWith(['/games/1'], {
+				relativeTo: activatedRouteMock,
+			});
+		});
 
-    it('should navigate to the game details page when not in /home route', () => {
-      component.gameInfo = { id: 1, name: 'Test Game', released: '2024-01-01' };
-      routerMock.navigate.mockResolvedValue(true);
-      /* eslint-disable   @typescript-eslint/ban-ts-comment */
-      //@ts-ignore
-      component.router.url = '/other';
+		it('should navigate to the game details page when not in /home route', () => {
+			component.gameInfo = { id: 1, name: 'Test Game', released: '2024-01-01' };
+			routerMock.navigate.mockResolvedValue(true);
+			/* eslint-disable   @typescript-eslint/ban-ts-comment */
+			//@ts-ignore
+			component.router.url = '/other';
 
-      component.goToGameDetails();
+			component.goToGameDetails();
 
-      expect(routerMock.navigate).toHaveBeenCalledWith([1], { relativeTo: activatedRouteMock });
-    });
-  });
+			expect(routerMock.navigate).toHaveBeenCalledWith([1], {
+				relativeTo: activatedRouteMock,
+			});
+		});
+	});
 });
