19dd2a4a803646f4a22a22d65bca39a456854f88
andrii2706 <andriipas200017@gmail.com>
2024-12-10 18:27:00 +0200

    clean up from any

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -50,7 +50,7 @@ export class AuthService {
 		localStorage.setItem('loggedIn', 'false');
 	}
 	/* eslint-disable  @typescript-eslint/no-explicit-any */
-	changeLoginStatus(status: boolean, userInfo: any) {
+	changeLoginStatus(status: boolean, userInfo: User| null) {
 		this.loggedInStatus = status;
 		localStorage.setItem('loggedIn', `${this.loggedInStatus}`);
 		if (userInfo) {
